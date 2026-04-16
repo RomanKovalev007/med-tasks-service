4b3cf1abec6c0e93ee9d5acb845cb0fb126b8f86
RomanKovalev007 <romakov640@gmail.com>
2026-04-16 13:04:25 +0300

    change repeatrule -> *repeatrule

--- internal/transport/http/handlers/dto.go
+++ internal/transport/http/handlers/dto.go
@@ -20,27 +20,30 @@ type taskUpdateStatusDTO struct {
 }
 
 type taskDTO struct {
-	ID            int64                 `json:"id"`
-	Title         string                `json:"title"`
-	Description   string                `json:"description"`
-	ScheduledAt   string                `json:"scheduled_at"`
-	Status        taskdomain.Status     `json:"status"`
-	IsPeriodicity bool                  `json:"is_periodicity"`
-	RepeatRule    taskdomain.RepeatRule `json:"repeat_rule,omitempty"`
-	CreatedAt     time.Time             `json:"created_at"`
-	UpdatedAt     time.Time             `json:"updated_at"`
+	ID            int64                  `json:"id"`
+	Title         string                 `json:"title"`
+	Description   string                 `json:"description"`
+	ScheduledAt   string                 `json:"scheduled_at"`
+	Status        taskdomain.Status      `json:"status"`
+	IsPeriodicity bool                   `json:"is_periodicity"`
+	RepeatRule    *taskdomain.RepeatRule `json:"repeat_rule,omitempty"`
+	CreatedAt     time.Time              `json:"created_at"`
+	UpdatedAt     time.Time              `json:"updated_at"`
 }
 
 func newTaskDTO(task *taskdomain.Task) taskDTO {
-	return taskDTO{
+	dto := taskDTO{
 		ID:            task.ID,
 		Title:         task.Title,
 		Description:   task.Description,
 		ScheduledAt:   task.ScheduledAt.Format(time.RFC3339),
 		Status:        task.Status,
 		IsPeriodicity: task.IsPeriodicity,
-		RepeatRule:    task.RepeatRule,
 		CreatedAt:     task.CreatedAt,
 		UpdatedAt:     task.UpdatedAt,
 	}
+	if task.IsPeriodicity {
+		dto.RepeatRule = &task.RepeatRule
+	}
+	return dto
 }
